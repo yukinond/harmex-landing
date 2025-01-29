e40fd34f4e07867e16cce3fd6238b85bc8c4cfbe
unknown <aqdcchh@gmail.com>
2025-01-30 03:18:28 +0500

    test1

--- pages/info/[slug].vue
+++ pages/info/[slug].vue
@@ -210,9 +210,22 @@ onUnmounted(() => {
   window.removeEventListener('resize', getBlockHeight);
 });
 
-const dynamicHeight = computed(() => ({
-  height: blockHeight.value !== 0 ? `${blockHeight.value + 150}px` : '800px'
-}));
+async function dynamicHeight(index:number) {
+  await nextTick(); 
+  if (contentBlock.value && contentBlock.value.length > 0) {
+    const currentBlock = contentBlock.value[index];
+    console.log('firstBlock', currentBlock)
+    if (currentBlock) {
+      console.log('firstBlock.offsetHeight', currentBlock.offsetHeight)
+      blockHeight.value = currentBlock.offsetHeight; 
+    }
+  }
+
+  return blockHeight.value
+}
+// const dynamicHeight = computed(() => ({
+//   height: blockHeight.value !== 0 ? `${blockHeight.value + 150}px` : '800px'
+// }));
 
 
 
@@ -265,16 +278,16 @@ onMounted(() => {
           </div>
         </div>
 
-        <div id="slideshow"  class="relative overflow-visible min-h-[400px] w-full -mb-32 lg:-mb-0  sm:my-0 z-0" :style="blockHeight !== 0 ? { height: blockHeight+150 + 'px' } : {height: '800px'}" >
+        <div id="slideshow"  class="relative overflow-visible min-h-[400px] w-full sm:my-0 z-0" :style="blockHeight !== 0 ? { height: blockHeight+150 + 'px' } : {height: '800px'}" >
           <transition-group name="fade" tag="div">
             <div
               v-for="(slide, index) in content"
               :key="index"
               v-show="currentContent === slide.value"
-              class="absolute w-full h-full lg:h-[580px] xl:h-[100%] -mb-20"
+              class="absolute w-full h-full lg:h-[580px] xl:h-[100%]"
             >
               <div class="flex w-full justify-between lg:flex-row flex-col p-4 md:p-12 gap-4 md:gap-12 bg-[#F7F7F7] rounded-2xl overflow-hidden" ref="contentBlock" :style="blockHeight !== 0 ? { height: blockHeight + 'px' } : {}">          
-                <div class="flex flex-col gap-3 md:gap-8 lg:flex-1">
+                <div class="flex flex-col gap-3 md:gap-8">
                   <p class="text-[18px] leading-[24px] md:font-[600] md:text-[24px] md:leading-[29px]">Как эту задачу решил Harmex:</p>
                   <div class="flex gap-3 items-center" v-for="item in content.find(item => item.value === currentContent).points">
                     <div class="w-[20px] h-[20px] flex items-center justify-center">
@@ -287,16 +300,46 @@ onMounted(() => {
                     </p>
                   </div>
                 </div>
-                <div class="relative min-h-[300px] lg:w-[45%] lg:max-w-[566px] lg:max-h-full lg:h-full flex lg:my-auto overflow-hidden rounded-xl flex-1 lg:flex-none max-h-[312px]">
-                  <Nuxt-img 
-                    :src="`/img/buisnessBlock/${slide.value}.png`" 
-                    class="w-full h-full object-cover lg:object-contain rounded-xl"
-                  />
+                <div class="w-full lg:max-w-[40%] rounded-lg overflow-hidden flex items-center">
+                  <Nuxt-img :src="`/img/buisnessBlock/${slide.value}.png`" class="w-full min-h-[312px] lg:min-h-0 object-contain rounded-2xl" />
                 </div>
               </div>
             </div>
           </transition-group>
         </div>
+        <!-- <div id="slideshow" class="relative overflow-visible min-h-[400px] w-full sm:my-0 z-0">
+          <transition-group name="fade" tag="div">
+            <div
+              v-for="(slide, index) in content"
+              :key="index"
+              v-show="currentContent === slide.value"
+              class="absolute flex w-full justify-between lg:flex-row flex-col p-4 md:p-12 gap-4 md:gap-12 h-full lg:h-[580px] xl:h-[100%] bg-[#F7F7F7] rounded-2xl"
+              :style="{ height:  blockHeight + 'px' }"
+              ref="contentBlock"
+            >
+              <div class="flex flex-col gap-3 md:gap-8 lg:flex-1">
+                <p class="text-[18px] leading-[24px] md:font-[600] md:text-[24px] md:leading-[29px]">Как эту задачу решил Harmex:</p>
+                <div 
+                  class="flex gap-3 items-center" 
+                  v-for="item in content.find(item => item.value === currentContent)?.points ?? []"
+                >
+                  <div class="w-[20px] h-[20px] flex items-center justify-center">
+                    <Icon name="icon-park-solid:check-one" class="w-[20px] h-[20px] text-[--primary]"/>
+                  </div>
+                  <p class="text-[#5A556B] text-[12px] lg:text-[16px] leading-[24px] font-[500]">         
+                    <span>{{ item.split(':')[0] }}:</span>
+                    <br />
+                    <span>{{ item.split(':')[1].trim() }}</span> 
+                  </p>
+                </div>
+              </div>
+              <div class="max-w-[40%] rounded-lg overflow-hidden flex items-center">
+                <Nuxt-img :src="`/img/buisnessBlock/${slide.value}.png`" class="w-full object-contain rounded-2xl" />
+              </div>
+            </div>
+          </transition-group>
+        </div> -->
+
       </section>
 
 
