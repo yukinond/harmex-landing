a5e9b8140e71c1c1a2c8aa6559c0c82ff7b3907c
unknown <aqdcchh@gmail.com>
2025-01-30 01:14:51 +0500

    point

--- pages/info/[slug].vue
+++ pages/info/[slug].vue
@@ -196,9 +196,6 @@ const getBlockHeight = async () => {
   }
 };
 
-const dynamicStyle = computed(() => ({
-  height: blockHeight.value !== 0 ? `${blockHeight.value + 150}px` : "800px",
-}));
 onMounted(() => {
   persistedStore.current ? persistedStore.current : persistedStore.current = 'wildberries'
   store.info[persistedStore.current].title ? mp.value = store.info[persistedStore.current] : navigateTo('/')
@@ -223,7 +220,7 @@ onMounted(() => {
 
       currentContent.value = content[nextIndex].value;
     }
-  }, 10000);
+  }, 30000);
 });
 </script>
 
@@ -260,7 +257,7 @@ onMounted(() => {
           </div>
         </div>
 
-        <!-- <div id="slideshow"  class="relative overflow-hidden min-h-[400px] w-full -mb-32 lg:-mb-0  sm:my-0 z-0" :style="blockHeight !== 0 ? { height: blockHeight+150 + 'px' } : {height: '800px'}" >
+        <div id="slideshow"  class="relative overflow-hidden min-h-[400px] w-full -mb-32 lg:-mb-0  sm:my-0 z-0" :style="blockHeight !== 0 ? { height: blockHeight+150 + 'px' } : {height: '800px'}" >
           <transition-group name="fade" tag="div">
             <div
               v-for="(slide, index) in content"
@@ -269,39 +266,6 @@ onMounted(() => {
               class="absolute w-full h-full lg:h-[580px] xl:h-[100%] -mb-20"
             >
               <div class="flex w-full justify-between lg:flex-row flex-col p-4 md:p-12 gap-4 md:gap-12 bg-[#F7F7F7] rounded-2xl overflow-hidden" ref="contentBlock" :style="blockHeight !== 0 ? { height: blockHeight + 'px' } : {}">          
-                <div class="flex flex-col gap-3 md:gap-8 lg:flex-1">
-                  <p class="text-[18px] leading-[24px] md:font-[600] md:text-[24px] md:leading-[29px]">Как эту задачу решил Harmex:</p>
-                  <div class="flex gap-3 items-center" v-for="item in content.find(item => item.value === currentContent).points">
-                    <div class="w-[20px] h-[20px] flex items-center justify-center">
-                      <Icon name="icon-park-solid:check-one" class="w-[20px] h-[20px] text-[--primary]"/>
-                    </div>
-                    <p class="text-[#5A556B] text-[12px] lg:text-[16px] leading-[24px] font-[500]">         
-                      <span>{{ item.split(':')[0] }}:</span>
-                      <br />
-                      <span>{{ item.split(':')[1].trim() }}</span> 
-                    </p>
-                  </div>
-                </div>
-                <div class="relative min-h-[300px] lg:w-[45%] lg:max-w-[566px] lg:max-h-full lg:h-full flex lg:my-auto overflow-hidden rounded-xl flex-1 lg:flex-none max-h-[312px]">
-                  <Nuxt-img 
-                    :src="`/img/buisnessBlock/${slide.value}.png`" 
-                    class="w-full h-full object-cover lg:object-contain rounded-xl"
-                  />
-                </div>
-              </div>
-            </div>
-          </transition-group>
-        </div> -->
-
-        <div id="slideshow"  class="relative overflow-visible min-h-[400px] w-full sm:my-0 z-0" ref="contentBlock" :style="dynamicStyle" >
-          <transition-group name="fade" tag="div">
-            <div
-              v-for="(slide, index) in content"
-              :key="index"
-              v-show="currentContent === slide.value"
-              class="absolute w-full h-full lg:h-[580px] xl:h-[100%]"
-            >
-              <div class="flex w-full justify-between lg:flex-row flex-col p-4 md:p-12 gap-4 md:gap-12 bg-[#F7F7F7] rounded-2xl overflow-visible" >          
                 <div class="flex flex-col gap-3 md:gap-8 lg:flex-1">
                   <p class="text-[18px] leading-[24px] md:font-[600] md:text-[24px] md:leading-[29px]">Как эту задачу решил Harmex:</p>
                   <div class="flex gap-3 items-center" v-for="item in content.find(item => item.value === currentContent).points">
@@ -481,7 +445,7 @@ onMounted(() => {
 <style scoped>
 
 #slideshow > div { 
-  position: relative; 
+  position: absolute; 
   top: 10px; 
   left: 10px; 
   right: 10px; 
